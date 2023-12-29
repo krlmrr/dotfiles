b3c4a3b1591afce66101e8e8ca6b3cafec15e346
Karl Murray <karl@karlm.dev>
2023-12-29 12:19:08 -0600

    Get plugins working

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -1,9 +1,3 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
-
-set runtimepath^=~/.vim runtimepath+=~/.vim/after
-let &packpath=&runtimepath
-source ~/.vimrc
+return {
+  -- Hello World!
+}

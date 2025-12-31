e4b7a049921030ef7538102909b8a88d4222d596
Karl Murray <karl@karlm.dev>
2025-12-31 12:36:26 -0600

    Fix the cursorline

--- nvim/lua/custom/plugins/dashboard.lua
+++ nvim/lua/custom/plugins/dashboard.lua
@@ -82,9 +82,26 @@ return {
                         end
                     end
 
-                    -- If no MRU files, hide cursorline entirely
+                    -- If no MRU files, show cursorline on "empty files" and lock cursor
                     if max_line == min_line then
-                        vim.opt_local.cursorline = false
+                        local empty_line = nil
+                        for i, line in ipairs(lines) do
+                            if line:match('empty') then
+                                empty_line = i
+                                break
+                            end
+                        end
+                        if empty_line then
+                            vim.api.nvim_win_set_cursor(0, { empty_line, 0 })
+                            -- Block all cursor movement
+                            local opts = { buffer = bufnr, nowait = true, silent = true }
+                            vim.keymap.set('n', 'j', '<Nop>', opts)
+                            vim.keymap.set('n', 'k', '<Nop>', opts)
+                            vim.keymap.set('n', '<Down>', '<Nop>', opts)
+                            vim.keymap.set('n', '<Up>', '<Nop>', opts)
+                            vim.keymap.set('n', 'gg', '<Nop>', opts)
+                            vim.keymap.set('n', 'G', '<Nop>', opts)
+                        end
                         return
                     end
 

d5a768d89b4258887eeaa401e0d43bdde6b94ef1
Karl Murray <karl@vheissulabs.com>
2026-05-04 15:11:57 -0500

    Reinstall yabai SA on brewup
    Brew upgrades replace the yabai binary, which invalidates the loaded
    scripting addition until it's reinstalled. Folding the SA dance into
    brewup avoids manual recovery after each upgrade.

--- mac/zsh/aliases.sh
+++ mac/zsh/aliases.sh
@@ -3,5 +3,5 @@ export PATH="$HOME/Library/Application Support/Herd/bin:$PATH"
 
 alias a="php artisan"
 
-alias brewup="brew upgrade && brew upgrade --greedy && brew cleanup --prune=all && sudo bash ~/Code/dotfiles/mac/tcc-cleanup.sh && open /Applications/Raycast.app/"
+alias brewup="brew upgrade && brew upgrade --greedy && brew cleanup --prune=all && sudo yabai --uninstall-sa && sudo yabai --install-sa && sudo yabai --load-sa && yabai --restart-service && sudo bash ~/Code/dotfiles/mac/tcc-cleanup.sh && open /Applications/Raycast.app/"
 
